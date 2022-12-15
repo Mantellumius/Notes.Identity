f5616efd0036fe33eacc84a86ebdd6fe9e93901c
Mantellumius <mantellumius@gmail.com>
2022-12-15 07:19:12 +0500

    + Entity Framework + Database (sqlite)

--- Notes.Identity/Startup.cs
+++ Notes.Identity/Startup.cs
@@ -1,26 +1,60 @@
 using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
 using System.Threading.Tasks;
 using IdentityServer4.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Notes.Identity.Data;
+using Notes.Identity.Models;
 
 namespace Notes.Identity
 {
     public class Startup
     {
+        public IConfiguration AppConfiguration { get; }
+
+        public Startup(IConfiguration appConfiguration)
+        {
+            AppConfiguration = appConfiguration;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            var connectionString = AppConfiguration.GetValue<string>("DbConnection");
+            services.AddDbContext<AuthDbContext>(options => { options.UseSqlite(connectionString); });
+
+            services.AddIdentity<AppUser, IdentityRole>(config =>
+                {
+                    config.Password.RequiredLength = 6;
+                    config.Password.RequireDigit = false;
+                    config.Password.RequireNonAlphanumeric = false;
+                    config.Password.RequireUppercase = false;
+                })
+                .AddEntityFrameworkStores<AuthDbContext>()
+                .AddDefaultTokenProviders();
+
             services.AddIdentityServer()
+                .AddAspNetIdentity<AppUser>()
                 .AddInMemoryApiResources(Configuration.ApiResources)
                 .AddInMemoryIdentityResources(Configuration.IdentityResources)
                 .AddInMemoryApiScopes(Configuration.ApiScopes)
                 .AddInMemoryClients(Configuration.Clients)
                 .AddDeveloperSigningCredential();
+
+            services.ConfigureApplicationCookie(config =>
+            {
+                config.Cookie.Name = "Notes.Identity.Cookie";
+                config.LoginPath = "/Auth/Login";
+                config.LogoutPath = "/Auth/Logout";
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -32,7 +66,7 @@ namespace Notes.Identity
 
             app.UseRouting();
             app.UseIdentityServer();
-            
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/", async context => { await context.Response.WriteAsync("Hello World!"); });
