b62d1015998d84fd6ff3ae3cd5d04080158df21c
Mantellumius <mantellumius@gmail.com>
2022-12-15 06:52:25 +0500

    + Configuration

---
+++ Notes.Identity/Configuration.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using IdentityModel;
+using IdentityServer4;
+using IdentityServer4.Models;
+
+namespace Notes.Identity
+{
+    public static class Configuration
+    {
+        public static IEnumerable<ApiScope> ApiScopes => new List<ApiScope>
+        {
+            new ApiScope("NotesWebAPI", "Web API")
+        };
+
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+            };
+
+        public static IEnumerable<ApiResource> ApiResources => new List<ApiResource>
+        {
+            new ApiResource("NotesWebAPI", "Wev API", new[] { JwtClaimTypes.Name })
+            {
+                Scopes = { "NotesWebAPI" }
+            }
+        };
+
+        public static IEnumerable<Client> Clients =>
+            new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "notes-web-api",
+                    ClientName = "Notes Web",
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RequireClientSecret = false,
+                    RequirePkce = true,
+                    RedirectUris =
+                    {
+                        "http://.../signin-oidc"
+                    },
+
+                    AllowedCorsOrigins =
+                    {
+                        "http://..."
+                    },
+                    PostLogoutRedirectUris =
+                    {
+                        "https://.../signout-oidc"
+                    },
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "NotesWevAPI"
+                    },
+                    AllowAccessTokensViaBrowser = true,
+                }
+            };
+    }
+}
